1f15517d89bdd4850a0d7cd0f2ea47a7ef0516b3
freOn1131 <138793109+freOn1131@users.noreply.github.com>
2025-03-26 15:03:07 +0300

    Update common.py

--- handlers/common.py
+++ handlers/common.py
@@ -20,7 +20,6 @@ import torch
 
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
-from transformers.utils import shard_checkpoint
 
 router = Router()
 
